e701bb0cef5729e1127bfffb0e84acef0e2a7d3d
Muhammad Suleman <leodeveloper@gmail.com>
2024-06-18 06:25:46 +0400

    improve the performance

--- app.py
+++ app.py
@@ -20,21 +20,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
-llm = Groq(temperature=0,model="llama3-70b-8192")
+llm = Groq(temperature=0.5,model="llama3-8b-8192")
 Settings.llm = llm
 
-# check if storage already exists
-PERSIST_DIR = "./storage"
-if not os.path.exists(PERSIST_DIR):
-    # load the documents and create the index
-    documents = SimpleDirectoryReader("youtubetranscript/text/").load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    # store it for later
-    index.storage_context.persist(persist_dir=PERSIST_DIR)
-else:
-    # load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
+
 
 # Function to sort by publish_date
 def sort_by_publish_date(item):
@@ -50,19 +39,35 @@ def load_video_titles():
 
 # Function to simulate response from LLM model (replace with actual model logic)
 def generate_response(user_input):
+    # check if storage already exists
+    PERSIST_DIR = "./storage"
+    if not os.path.exists(PERSIST_DIR):
+        # load the documents and create the index
+        documents = SimpleDirectoryReader("youtubetranscript/text/").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+        # store it for later
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    else:
+        # load the existing index
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+
     query_engine = index.as_chat_engine(chat_mode="context", llm=llm,verbose=True)
     response = query_engine.chat(user_input)
     return response.response
 
 # Sidebar content
 st.sidebar.title('Zeeshan Usmani YouTube Channel')
-st.sidebar.image('https://yt3.googleusercontent.com/a-Sr9AQV5fW43HXMCV7FmFOb5ngJx3_jm7lsFf2q0MyM3-RUMQbW9Sa-2o8AizzGUjMRLO-wPQ=s160-c-k-c0x00ffffff-no-rj', use_column_width=True)
-#st.sidebar.header("YouTube Video's")
-#video_titles = load_video_titles()
-#for item in video_titles:
+st.sidebar.image('zusmani_channels4_profile.jpg', use_column_width=True)
+st.sidebar.header("YouTube Video's")
+video_titles = load_video_titles()
+count=1
+for item in video_titles:
     #st.sidebar.image(f"{item['thumbnail_url']}", use_column_width=True)
-#    st.sidebar.write(f"{item['title']}")
-#    st.sidebar.write(f"{item['publish_date']}")
+    st.sidebar.write(f"{count}.{item['title']}")
+    st.sidebar.write(f"{item['publish_date']}")
+    st.sidebar.write("-----------------------")
+    count+=1
 
 # Main content
 st.title('Zeeshan Usmani YouTube Channel Chatbot')
